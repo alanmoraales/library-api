ae9f125866b35b2d69824b521941e483847162ec
alanmoraales <alanmoraales845@gmail.com>
2022-05-21 14:43:40 -0500

    add database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from 'database';
+import config from 'config';
+import environments from 'environments';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: environments[process.env.NODE_ENV] || '.env',
+      load: [config],
+      isGlobal: true,
+    }),
+    DatabaseModule,
+  ],
 })
 export class AppModule {}
---
+++ src/database/database.module.ts
@@ -0,0 +1,61 @@
+import { Module, Global } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Client } from 'pg';
+import config from 'config';
+
+@Global()
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { user, host, dbName, password, port, url } =
+          configService.postgres;
+        const baseConfig = {
+          synchronize: false,
+          autoLoadEntities: true,
+        };
+        if (url) {
+          return {
+            type: 'postgres',
+            url,
+            ssl: { rejectUnauthorized: false },
+            ...baseConfig,
+          };
+        }
+        return {
+          type: 'postgres',
+          host,
+          port,
+          username: user,
+          password,
+          database: dbName,
+          ...baseConfig,
+        };
+      },
+    }),
+  ],
+  providers: [
+    {
+      provide: 'PG',
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { user, host, dbName, password, port, url } =
+          configService.postgres;
+        const dbConfig = url || {
+          user,
+          host,
+          database: dbName,
+          password,
+          port,
+        };
+        const client = new Client(dbConfig);
+        client.connect();
+        return client;
+      },
+      inject: [config.KEY],
+    },
+  ],
+  exports: ['PG', TypeOrmModule],
+})
+export class DatabaseModule {}
