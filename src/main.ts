7f5942e82121e2c07664849d611909bb31cfb269
alanmoraales <alanmoraales845@gmail.com>
2022-05-21 13:51:17 -0500

    add dynamic port, swagger, cors and class-validator setup config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+
+  // class-validator pipeline
+  app.useGlobalPipes(new ValidationPipe());
+
+  // swagger documentation
+  const config = new DocumentBuilder()
+    .setTitle('Library API Docs')
+    .setDescription(
+      'A Library API to create book reservations and picked them up with a QR code.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        description: 'Enter JWT for your user',
+      },
+      'jwt',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
