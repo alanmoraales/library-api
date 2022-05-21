1e67e7c0fef6f8f385b042864bf7613616023a21
alanmoraales <alanmoraales845@gmail.com>
2022-05-21 15:11:27 -0500

    add basic user module

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,34 @@
+import {
+  PrimaryColumn,
+  Entity,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryColumn()
+  id: number;
+
+  @Column({ type: 'varchar' })
+  name: string;
+
+  @Column({ type: 'varchar' })
+  email: string;
+
+  @Column({ type: 'varchar' })
+  password: string;
+
+  @CreateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+}
