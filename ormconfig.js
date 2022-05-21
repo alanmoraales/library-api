4403f595669757bdaa577a769b62d18a37cec04c
alanmoraales <alanmoraales845@gmail.com>
2022-05-21 15:35:47 -0500

    add typeorm migrations config

---
+++ ormconfig.js
@@ -0,0 +1,24 @@
+module.exports = {
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT, 10),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  synchronize: false,
+  entities: ['src/**/*.entity.ts'],
+  ssl: Boolean(process.env.DATABASE_URL),
+  extra: Boolean(process.env.DATABASE_URL)
+    ? {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : null,
+  migrationsTableName: 'migrations',
+  migrations: ['src/database/migrations/*.ts'],
+  cli: {
+    migrationsDir: 'src/database/migrations',
+  },
+};
