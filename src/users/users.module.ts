ff8a87e3c1e46a357f765b7b9f8f9f52a756ed1b
alanmoraales <alanmoraales845@gmail.com>
2022-05-21 23:47:59 -0500

    creating user cart after registration, resolving cart relation

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from './entities';
+import { Cart } from 'carts/entities';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Cart])],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService],
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Cart } from 'carts/entities';
 import { Repository } from 'typeorm';
 import { User } from './entities';
 import { ICreateUser } from './interfaces';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectRepository(User) private usersRepo: Repository<User>) {}
+  constructor(
+    @InjectRepository(User) private usersRepo: Repository<User>,
+    @InjectRepository(Cart) private cartRepo: Repository<Cart>,
+  ) {}
 
   async isRegisteredEmail(email: string) {
     const foundUsers = await this.usersRepo.find({ where: { email } });
@@ -15,11 +19,17 @@ export class UsersService {
 
   async createAndSaveUser(userPayload: ICreateUser) {
     const user = this.usersRepo.create(userPayload);
+    const userCart = this.cartRepo.create();
+    await this.cartRepo.save(userCart);
+    user.cart = userCart;
     return await this.usersRepo.save(user);
   }
 
   async findOneByEmail(email: string) {
-    const foundUsers = await this.usersRepo.find({ where: { email } });
+    const foundUsers = await this.usersRepo.find({
+      where: { email },
+      relations: ['cart'],
+    });
     return foundUsers[0];
   }
 }
