188b8a2f865bfe61fc86ca45caf4235a63fcd8ad
alanmoraales <alanmoraales845@gmail.com>
2022-11-08 19:58:04 -0600

    add books resolver

--- src/books/books.resolver.ts
+++ src/books/books.resolver.ts
@@ -1,4 +1,4 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Query, Resolver, Args } from '@nestjs/graphql';
 import { BooksService } from './books.service';
 import { Book } from './entities';
 
@@ -7,7 +7,24 @@ export class BooksResolver {
   constructor(private booksService: BooksService) {}
 
   @Query(() => [Book])
-  async books() {
+  async books(
+    @Args('page') page?: number,
+    @Args('limit') limit?: number,
+    @Args('search') search?: string,
+  ) {
+    if (search) {
+      return this.booksService.searchPaginatedBooks({
+        page: String(page),
+        limit: String(limit),
+        search,
+      });
+    }
+    if (page) {
+      return this.booksService.getPaginatedBooks({
+        page: String(page),
+        limit: String(limit),
+      });
+    }
     return this.booksService.getAllBooks();
   }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,21 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { paginate, Pagination } from 'nestjs-typeorm-paginate';
 import { Book } from './entities';
+import { PaginatedBooksQueryDto } from './dtos';
 
 @Injectable()
 export class BooksService {
-  constructor(@InjectRepository(Book) private booksRepo: Repository<Book>) {}
+  constructor(
+    @InjectRepository(Book) private booksRepository: Repository<Book>,
+  ) {}
 
   async getAllBooks() {
-    return await this.booksRepo.find();
+    return await this.booksRepository.find();
+  }
+
+  async searchPaginatedBooks({
+    page = '1',
+    limit = '10',
+    search,
+  }: PaginatedBooksQueryDto): Promise<Pagination<Book>> {
+    const queryBuilder = this.booksRepository
+      .createQueryBuilder()
+      .select()
+      .where('title ILIKE :searchTerm', { searchTerm: `%${search}%` });
+    return paginate<Book>(queryBuilder, { page, limit });
+  }
+
+  async getPaginatedBooks({
+    page = '1',
+    limit = '10',
+  }: PaginatedBooksQueryDto) {
+    return paginate<Book>(this.booksRepository, { page, limit });
   }
 
   async findOneById(bookId: number) {
-    return await this.booksRepo.findOne(bookId);
+    return await this.booksRepository.findOne(bookId);
   }
 
   async saveBook(book: Book) {
-    return await this.booksRepo.save(book);
+    return await this.booksRepository.save(book);
   }
 }
