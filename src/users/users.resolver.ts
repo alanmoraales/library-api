fe85cdc90184429480140e2cac4b07c7b089f6e8
alanmoraales <alanmoraales845@gmail.com>
2022-11-14 18:46:07 -0600

    add remove and add book to cart mutations

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,6 +1,7 @@
-import { UseGuards } from '@nestjs/common';
-import { Context, Resolver, Query } from '@nestjs/graphql';
+import { BadRequestException, UseGuards } from '@nestjs/common';
+import { Context, Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { JwtGraphQLGuard } from 'auth/guards';
+import { BooksService } from 'books/books.service';
 import { Cart } from 'carts/entities';
 import { Request as ExpressRequest } from 'express';
 import { User } from './entities';
@@ -9,7 +10,10 @@ import { UsersService } from './users.service';
 
 @Resolver()
 export class UsersResolver {
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private booksService: BooksService,
+  ) {}
 
   @Query(() => UserResponse)
   @UseGuards(JwtGraphQLGuard)
@@ -23,5 +27,60 @@ export class UsersResolver {
     const user = req.user as User;
     const userCart = await this.userService.findUserCart(user.id);
     return userCart;
+  }
+
+  @Mutation(() => Cart)
+  @UseGuards(JwtGraphQLGuard)
+  async addBookToCart(
+    @Context('req') req: ExpressRequest,
+    @Args('bookId') bookId: number,
+    @Args('quantity') quantity: number,
+  ) {
+    const user = req.user as User;
+    const book = await this.booksService.findOneById(bookId);
+    if (!(quantity <= book.availableQuantity)) {
+      throw new BadRequestException(
+        'No hay suficientes piezas disponibles de este libro',
+      );
+    }
+    const userCart = await this.userService.findUserCart(user.id);
+    const cartItemWithBookInCart = userCart.items.find(
+      (cartItem) => cartItem.book.id === bookId,
+    );
+    const cartItem =
+      cartItemWithBookInCart ||
+      (await this.userService.createAndSaveCartItem({
+        book,
+        cart: userCart,
+        quantity,
+      }));
+    if (cartItemWithBookInCart) {
+      cartItem.quantity = cartItem.quantity + quantity;
+      await this.userService.saveCartItem(cartItem);
+    }
+    book.availableQuantity = book.availableQuantity - quantity;
+    await this.booksService.saveBook(book);
+    return await this.userService.findUserCart(user.id);
+  }
+
+  @Mutation(() => Cart)
+  @UseGuards(JwtGraphQLGuard)
+  async removeBookFromCart(
+    @Context('req') req: ExpressRequest,
+    @Args('bookId') bookId: number,
+  ) {
+    const user = req.user as User;
+    const userCart = await this.userService.findUserCart(user.id);
+    const cartItemWithBookInCart = userCart.items.find(
+      (cartItem) => cartItem.book.id === bookId,
+    );
+    if (cartItemWithBookInCart) {
+      const book = await this.booksService.findOneById(bookId);
+      book.availableQuantity =
+        book.availableQuantity + cartItemWithBookInCart.quantity;
+      await this.booksService.saveBook(book);
+      await this.userService.removeCartItem(cartItemWithBookInCart);
+    }
+    return await this.userService.findUserCart(user.id);
   }
 }
