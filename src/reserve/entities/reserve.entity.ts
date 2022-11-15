5fa89663100e3ac299d7b9eb23a306ca2d508f69
alanmoraales <alanmoraales845@gmail.com>
2022-11-14 19:17:47 -0600

    add reserve entity to ggraphql schema

--- src/reserve/entities/reserve.entity.ts
+++ src/reserve/entities/reserve.entity.ts
@@ -10,30 +10,37 @@ import {
 import { CartItem } from '../../carts/entities';
 import { User } from '../../users/entities';
 import { EReserveStatus } from '../../reserve/enums';
+import { Field, Int, ObjectType } from '@nestjs/graphql';
 
 @Entity()
+@ObjectType()
 export class Reserve {
   @PrimaryGeneratedColumn()
+  @Field(() => Int)
   id: number;
 
   @ManyToOne(() => User)
   user: User;
 
   @OneToMany(() => CartItem, (cartItem) => cartItem.reserve)
+  @Field(() => [CartItem])
   items: CartItem[];
 
   @Column({ type: 'varchar', default: EReserveStatus.PENDING_FOR_RECOLLECTION })
+  @Field()
   status: EReserveStatus;
 
   @CreateDateColumn({
     type: 'timestamptz',
     default: () => 'CURRENT_TIMESTAMP',
   })
+  @Field()
   createdAt: Date;
 
   @UpdateDateColumn({
     type: 'timestamptz',
     default: () => 'CURRENT_TIMESTAMP',
   })
+  @Field()
   updatedAt: Date;
 }
